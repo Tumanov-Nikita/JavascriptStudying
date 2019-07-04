0ef97a8524f91c2b81db7c54ce26f1626b6077e6
Tumanov-Nikita <33156396+Tumanov-Nikita@users.noreply.github.com>
2019-07-04 20:01:25 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,57 @@
-alert('Hello Mediasoft')
+alert('Результат: ' + Parser());
+
+
+function Parser() {
+    let str = prompt('Введите выражение', '');
+    let answer;
+    const notCorrect = 'по крайней мере один из операндов или оператор введены некорректно';
+    if (~str.indexOf('/')) {
+        let expression = str.split('/');
+        if (CheckingVars(expression)) {
+            answer = +expression[0] / +expression[1];
+        } else {
+            answer = notCorrect;
+        }
+        return answer;
+    }
+    else if (~str.indexOf('*')) {
+        let expression = str.split('*');
+        if (CheckingVars(expression)) {
+            answer = +expression[0] * +expression[1];
+        } else {
+            answer = notCorrect;
+        }
+        return answer;
+    }
+    else if (~str.indexOf('+')) {
+        let expression = str.split('+');
+        if (CheckingVars(expression)) {
+            answer = +expression[0] + +expression[1];
+        } else {
+            answer = notCorrect;
+        }
+        return answer;
+    }
+    else if (~str.indexOf('-')) {
+        let temp = str.match(/.+?-/);
+        temp = String(temp);
+        let var1 = temp.slice(0,-1);
+        let var2 = str.slice(var1.length+1);
+        let expression = new Array();
+        expression[0]=var1;
+        expression[1]=var2;
+        if (CheckingVars(expression)) {
+            answer = +expression[0] - +expression[1];
+        } else {
+            answer = notCorrect;
+        }
+        return answer;
+    } else {
+        answer = 'выражение введено неверно. Допускаются операции сложения, вычитания, умножения и деления между двумя операндами';
+        return answer;
+    }
+}
+
+function CheckingVars(variables){
+    return (!isNaN(+variables[0]) && !isNaN(+variables[1])) ? true : false;
+}
