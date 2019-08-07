f021e280a1d14db916e91cce22c58a3e5d93d3ff
Tumanov-Nikita <33156396+Tumanov-Nikita@users.noreply.github.com>
2019-08-07 17:31:02 +0400

    Design rework

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const cellCountWidth = 10;
 const cellCountHeight = 10;
 const MakingColumns = ['','А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ж', 'З', 'И', 'К'];
+let tablePlayer;
+let tablePlayer2;
 
-function GameField() {
-    let tablePlayer1 = document.createElement('table');
+function ClearPage() {
+    tablePlayer1.remove();
+    tablePlayer2.remove();
+}
+
+function DrawTable(){
+    tablePlayer1 = document.createElement('table');
     tablePlayer1.className = 'gameField';
     for (let i = 0; i <= cellCountHeight; i++) {
         let row = document.createElement('tr');
@@ -30,40 +37,23 @@ function GameField() {
         }
     }
     document.body.appendChild(tablePlayer1);
-
-    let tablePlayer2 = document.createElement('table');
-    tablePlayer2.className = 'gameField';
-    for (let i = 0; i <= cellCountHeight; i++) {
-        let row = document.createElement('tr');
-        tablePlayer2.appendChild(row);
-        for (let j = 0; j <= cellCountWidth; j++) {
-            let cell = document.createElement('td');
-            if (i!=0 && j!=0){ 
-                cell.className = 'waterCell';     
-            }
-            else if (j==0 && i!=0){
-                cell.className = 'markingCell';                
-                let cellText = document.createElement('p');
-                cellText.innerHTML = i;
-                cell.appendChild(cellText);
-            }
-            else{
-                cell.className = 'markingCell';                
-                let cellText = document.createElement('p');
-                cellText.innerHTML = MakingColumns[j];
-                cell.appendChild(cellText);
-            }
-            
-            tablePlayer2.appendChild(cell);
-        }
-    }
-    document.body.appendChild(tablePlayer2);
+    tablePlayer2 = tablePlayer1.cloneNode(true);
+    tablePlayer1.after(tablePlayer2);
 }
 
-GameField();
+let footer = document.createElement('div');
+footer.className = 'footer';
 
 let buttonRestart = document.createElement('button');
 buttonRestart.className = '.buttonRestart';
-buttonRestart.onclick = GameField();
+buttonRestart.addEventListener("click", ClearPage);
+buttonRestart.addEventListener("click", DrawTable);
 buttonRestart.innerHTML = 'Начать заново';
-document.body.appendChild(buttonRestart);
+footer.appendChild(buttonRestart);
+
+document.body.appendChild(footer);
+
+DrawTable();
+
+
+
